2e0d62d7dc1366a8cf7ba40a7ce30f4b8157dd8e
Alvaro <alvaro.nrx88@gmail.com>
2024-04-26 02:44:23 -0300

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'mks-sistemas.nyc3',
+                port: '',
+                pathname: '/products/ipadair.webp',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
